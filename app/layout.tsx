ba648b25bbe439e251765159084b3bac312bfaea
Yash Dhadge <yash.dhadge10@gmail.com>
2026-04-07 14:26:48 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,8 +40,8 @@ export const metadata: Metadata = {
   },
 
   icons: {
-    icon: [{ url: "/favicon.ico" }, { url: "/icon.png", type: "image/png" }],
-    apple: { url: "/apple-touch-icon.png" },
+    icon:  { url: "/icon.png", type: "image/png" },
+    apple: { url: "/icon.png", type: "image/png" },
   },
 
   alternates: {
